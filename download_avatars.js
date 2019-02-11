8abdaba283eef4fdc06e3c4379601d6e9a8a0f79
Ken Thooptong <ken.thooptong@gmail.com>
2019-02-11 23:40:54 +0000

    adding authentication in the header. Token kept in a secret file

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,26 @@
+var secret = require('./secret.js')
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : 'token ' + secret.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
+
+
+
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
